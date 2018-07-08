cb8dc43be7f1f72dd6e7d1e55f83fe781ac4b477
Mark Kho <markho90@gmail.com>
2018-07-08 22:42:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
+<<<<<<< HEAD
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
   return "I can\'t hear you!";
@@ -25,3 +26,14 @@ function sayHiToGrandma(string){
   return "I love you, too.";
   }
 }
+=======
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
+>>>>>>> 610de581c23ffbf2e99e251d7cc1ec0e5b19c9ab
